21c17662515e81e14db4bb6ad1cc61d1a64ddda8
phmok1@naver.com <phmok1@naver.com>
2025-03-13 14:32:58 +0900

    간단한 api 호출

--- hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -14,9 +15,37 @@ public class HelloController {
         return "hello";
     }
 
+    // http://localhost:8080/hello-string?name=아무개
     @GetMapping("hello-mvc")
-    public String hellMvc(@RequestParam("name") String name, Model model ) {
+    public String hellMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    // http://localhost:8080/hello-string?name=아무개
+    @GetMapping("hello-string")
+    @ResponseBody // 응답 body 부분에 직접  넣어주겠다.
+    public String helloString(@RequestParam("name") String name) {
+        return "안녕 " + name;
+    }
+    // http://localhost:8080/hello-api?name=아무개
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String userName) {
+        Hello helloInstance = new Hello();
+        helloInstance.setName(userName);
+        return helloInstance;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
