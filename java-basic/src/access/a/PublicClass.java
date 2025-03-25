a2fc65229cc57f31aec49b92c3e908d4f48106b0
phmok1@naver.com <phmok1@naver.com>
2025-03-25 17:03:53 +0900

    접근제어자

---
+++ java-basic/src/access/a/PublicClass.java
@@ -0,0 +1,18 @@
+package access.a;
+
+public class PublicClass {
+    public static void main(String[] args) {
+        PublicClass publicClass = new PublicClass();
+        DefaultClass1 class1 = new DefaultClass1();
+        DefaultClass2 class2 = new DefaultClass2();
+    }
+}
+
+// default 접근 제이자
+class DefaultClass1 {
+
+}
+
+class DefaultClass2 {
+
+}
